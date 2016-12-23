ef7d76f706f80131ac7a763798cffa82cb9a6bf6
Bob Schriver <bobschriver@gmail.com>
2016-12-22 16:43:47 -0800

    Adding first test for cross pattern with nadir and oblique on realisticrendering app

---
+++ unrealcv_test.py
@@ -0,0 +1,57 @@
+from unrealcv import client
+
+# X runs east-west
+# Y runs north-south
+
+ROOM_LENGTH = 750 #500 to -250
+ROOM_WIDTH = 300 #-150 to 150
+ROOM_HEIGHT = 0.0
+
+STARTING_X = -150.0
+STARTING_Y = 500.0
+STARTING_Z = 200.0
+
+X_TRANSLATION = 50
+Y_TRANSLATION = 50
+
+heading_north = True
+
+client.connect() # Connect to the game
+if not client.isconnected(): # Check if the connection is successfully established
+  print('UnrealCV server is not running. Run the game from http://unrealcv.github.io first.')
+else:
+	yaw = 0.0
+	z_location = STARTING_Z
+	for x_offset in range(0, ROOM_WIDTH + 1, X_TRANSLATION):
+		x_location = STARTING_X + (x_offset)
+		
+		if heading_north:
+			pitch = 0.0
+			starting_y_range = 0
+			ending_y_range = ROOM_LENGTH + 1
+			translation_y_range = Y_TRANSLATION
+			heading_north = False
+		else:
+			pitch = 180.0
+			starting_y_range = ROOM_LENGTH
+			ending_y_range = 0 - 1
+			translation_y_range = -1 * Y_TRANSLATION
+			heading_north = True
+
+	
+		for y_offset in range(starting_y_range, ending_y_range, translation_y_range):
+			y_location = STARTING_Y + (-1 * y_offset)
+			
+			cmd = 'vset /camera/0/moveto %.1f %.1f %.1f' % (x_location, y_location, z_location)
+			response = client.request(cmd)
+			
+			cmd = 'vset /camera/0/rotation %.1f %.1f %.1f' % (270, pitch, yaw)
+			response = client.request(cmd)
+						
+			response = client.request('vget /camera/0/lit')
+			print response
+			
+			cmd = 'vset /camera/0/rotation %.1f %.1f %.1f' % (300, pitch, yaw)
+			response = client.request(cmd)
+
+  
